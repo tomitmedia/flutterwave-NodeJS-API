eacb1ece630d1cf35cef4c9b66f990e98d132130
Oluwafemi_Tom <Oluwafemi_Tom@Oluwafemi-Tom.local>
2022-07-20 19:35:38 +0100

    second commit

--- index.js
+++ index.js
@@ -1,86 +1,76 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 app.post("/split-payments/compute", (req, res) => {
-    const {ID, Amount, Currency, CustomerEmail, SplitInfo} = req.body
-    let comp = Amount
-    let flat
-    let percent
-    let ratio
+  const { ID, Amount, Currency, CustomerEmail, SplitInfo } = req.body;
+  let comp = Amount;
+  let flat;
+  let percent;
+  let ratio;
 
-    for(let i = 0; i < SplitInfo.length; i++ ){
+  for (let i = 0; i < SplitInfo.length; i++) {
+    if (SplitInfo[i].SplitType === "FLAT") {
+      flat = SplitInfo[i];
 
+      comp = comp - flat.SplitValue;
 
+      let Flat = {
+        SplitEntityId: flat.SplitEntityId,
+        Amount: flat.SplitValue,
+      };
 
-        if(SplitInfo[i].SplitType === 'FLAT'){
-            flat = SplitInfo[i]
-        
-            comp = comp - flat.SplitValue
-            
-            let Flat = {
-                SplitEntityId: flat.SplitEntityId,
-                Amount: flat.SplitValue
-            }
+      SplitInfo[i] = Flat;
+      console.log("flat = " + flat.SplitValue);
+    } else if (SplitInfo[i].SplitType === "PERCENTAGE") {
+      percent = SplitInfo[i];
+      let cent;
 
-            SplitInfo[i] = Flat
-            console.log('flat = ' +flat.SplitValue)
+      cent = (percent.SplitValue / 100) * comp;
+      comp = comp - cent;
 
+      let Percent = {
+        SplitEntityId: percent.SplitEntityId,
+        Amount: cent,
+      };
 
-        }else if(SplitInfo[i].SplitType === 'PERCENTAGE'){
-            percent = SplitInfo[i]
-            let cent
+      SplitInfo[i] = Percent;
 
-            cent = ((percent.SplitValue/100) * comp)
-            comp = comp - cent
+      console.log("percent = " + cent);
+    } else if (SplitInfo[i].SplitType === "RATIO") {
+      ratio = SplitInfo[i];
 
-            let Percent = {
-                SplitEntityId: percent.SplitEntityId,
-                Amount: cent
-            }
+      let total;
+      total = ratio.SplitValue + 0;
+      rat = (ratio.SplitValue / total) * comp;
 
-            SplitInfo[i] = Percent
+      comp = comp - rat;
 
-            console.log('percent = ' +cent)
-            
-        }else if (SplitInfo[i].SplitType === 'RATIO'){
-            ratio = SplitInfo[i]
+      let Ratio = {
+        SplitEntityId: ratio.SplitEntityId,
+        Amount: rat,
+      };
 
-            let total
-            total = ratio.SplitValue + 0;
-            rat = (ratio.SplitValue/total) * comp
-
-            comp = comp - rat
-
-            let Ratio = {
-                SplitEntityId: ratio.SplitEntityId,
-                Amount: rat
-            }
-
-            SplitInfo[i] = Ratio
-            console.log('ratio = ' +rat)
-
-        }
-        
+      SplitInfo[i] = Ratio;
+      console.log("ratio = " + rat);
     }
+  }
 
+  // console.log(SplitInfo)
+  // console.log(comp)
 
-    // console.log(SplitInfo)
-    // console.log(comp)
+  let result = {
+    ID,
+    Balance: comp,
+    SplitBreakdown: SplitInfo,
+  };
 
-    let result = {
-        ID,
-        Balance: comp,
-        SplitBreakdown: SplitInfo
-    }
-
-    res.send(result).status(200)
-
-})
+  res.send(result).status(200);
+});
 
 app.listen(port, () => {
-    console.log('server on port' +port)
-})
+  console.log("server on port" + port);
+});
